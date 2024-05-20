60b0d171460b6f29b80fce047f77f4dd8145498f
Ayesha Chishti <chishtiayesha@gmail.com>
2024-05-20 23:26:35 +0100

    Create a modular framework for UI tests with logging and configuration (Scenario 4)

---
+++ utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+def setup_logger(name, log_file, level=logging.INFO):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+test_logger = setup_logger('test_logger', 'test.log')
